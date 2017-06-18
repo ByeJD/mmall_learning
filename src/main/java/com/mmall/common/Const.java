2f7f710379efabe3fe60f90da10a0423b3367991
ByeJD <15988855324@163.com>
2017-06-18 23:47:30 +0800

    init

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,17 @@
+package com.mmall.common;
+
+/**
+ * Created by ADMIN on 2017/6/18.
+ */
+public class Const {
+
+    public static  final  String CURRENT_USER = "current_user";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public  interface  Role{
+        int ROLE_CUSTOMER=0;
+        int ROLE_ADMIN=1;
+    }
+}
